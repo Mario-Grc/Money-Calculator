7d6c0c2da4819a624358c6d6c53fc64894d02cec
Mario <mario2004mario16@hotmail.com>
2024-12-05 22:12:14 +0000

    Minor fixes

--- src/main/java/software/ulpgc/moneycalculator/io/CurrencyDeserializer.java
+++ src/main/java/software/ulpgc/moneycalculator/io/CurrencyDeserializer.java
@@ -1,7 +1,5 @@
 package software.ulpgc.moneycalculator.io;
 
-import software.ulpgc.moneycalculator.model.Currency;
-
 public interface CurrencyDeserializer {
     Object deserialize(String read);
 }
